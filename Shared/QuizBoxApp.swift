757a9658ede9cfdf65a48a5ef3ce93557be6ee98
Jonathan <Jonathanccrosby214@gmail.com>
2021-12-04 10:39:35 -0500

    Initial Commit

---
+++ Shared/QuizBoxApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuizBoxApp.swift
+//  Shared
+//
+//  Created by Jonathan Crosby on 12/4/21.
+//
+
+import SwiftUI
+
+@main
+struct QuizBoxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
